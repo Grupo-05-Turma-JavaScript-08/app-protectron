c2e3f04552cd9e69c6beaab3801a7007cfbf2f11
gmartins9042 <gmartins9042@gmail.com>
2025-08-01 17:54:14 -0300

    feat: Finaliza o Crud Electronic

--- src/electronic/controller/electronic.controller.ts
+++ src/electronic/controller/electronic.controller.ts
@@ -1,26 +1,42 @@
-import { Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { ElectronicService } from "../services/electronic.service";
 import { Electronic } from "../entites/electronic.entity";
 
 @Controller('electronic')
 export class ElectronicController {
-  constructor(private readonly electronicService: ElectronicService){}
+    constructor(private readonly electronicService: ElectronicService) { }
 
-  @Get()
-  @HttpCode(HttpStatus.OK)
-   findAll(): Promise<Electronic[]> {
-    return this.electronicService.findAll();
-}
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Electronic[]> {
+        return this.electronicService.findAll();
+    }
 
-@Get('/:id')
-@HttpCode(HttpStatus.OK)
-findById(@Param('id', ParseIntPipe) id: number): Promise<Electronic> {
-    return this.electronicService.findById(id);
-}
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Electronic> {
+        return this.electronicService.findById(id);
+    }
 
-@Get('/model/:model')
-@HttpCode(HttpStatus.OK)
-findAllByModel(@Param('model') model: string): Promise<Electronic[]> {
-    return this.electronicService.findAllByModel(model);
-}
+    @Get('/model/:model')
+    @HttpCode(HttpStatus.OK)
+    findAllByModel(@Param('model') model: string): Promise<Electronic[]> {
+        return this.electronicService.findAllByModel(model);
+    }
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() electronic: Electronic): Promise<Electronic> {
+        return this.electronicService.create(electronic);
+    }
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() electronic: Electronic): Promise<Electronic> {
+        return this.electronicService.update(electronic);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number) {
+        return this.electronicService.delete(id);
+    }
 }
--- src/electronic/services/electronic.service.ts
+++ src/electronic/services/electronic.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Electronic } from "../entites/electronic.entity";
-import { ILike, Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 
 @Injectable()
 export class ElectronicService {
@@ -22,9 +22,24 @@ export class ElectronicService {
         return electronic;
     }
     async findAllByModel(model: string): Promise<Electronic[]> {
-return await this.electronicRepository.find({
-    where: {
-       model : ILike(`%${model}%`)
+        return await this.electronicRepository.find({
+            where: {
+                model: ILike(`%${model}%`)
+            }
+        })
     }
-})}
+    async create(electronic: Electronic): Promise<Electronic> {
+        return await this.electronicRepository.save(electronic);
+    }
+    async update( electronic: Electronic): Promise<Electronic> {
+        if (!electronic.id)
+            throw new HttpException('Id de produto precisa ser informado!', HttpStatus.NOT_FOUND);
+        await this.findById(electronic.id);
+        return await this.electronicRepository.save(electronic);
+    }
+    async delete(id: number): Promise<DeleteResult> {
+        await this.findById(id);
+        return await this.electronicRepository.delete(id);
+    
+    }
 }
